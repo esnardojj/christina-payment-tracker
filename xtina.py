f4cae5e1c7ccfb6234ae0fcf54dea8cb3aebadd1
julesnardo <esnardojuliet@gmail.com>
2021-01-06 17:16:04 -0500

    Initial commit

---
+++ xtina.py
@@ -0,0 +1,8 @@
+# christina owes u this much
+cost = 1146
+
+# func to print out how much christina owes me
+paid = float(input("How much money have you paid: "))
+
+unpaid = float(cost - paid)
+print("You owe ", unpaid)
