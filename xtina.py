302bf6dd6af5ddd84a9c4a8fd30aef04b10917ee
julesnardo <esnardojuliet@gmail.com>
2021-01-07 08:46:01 -0500

    updates

--- xtina.py
+++ xtina.py
@@ -1,8 +1,24 @@
-# christina owes u this much
+import datetime
+
+# Set date
+date = datetime.datetime.now()
+
+# Set grand total cost
 cost = 1146
-
-# func to print out how much christina owes me
-paid = float(input("How much money have you paid: "))
-
+paid = float(146)
 unpaid = float(cost - paid)
-print("You owe $", unpaid)
+
+# Set payments
+payments = str(1)
+
+# Greeting everytime
+greeting = "Hi Christina! Would you like to see your payment track? (Y/N) "
+see_details = input(greeting)
+
+if see_details == 'y' or see_details == 'Y':
+    update = "As of " + date.strftime("%x")+":\nYou have made " + payments + " payments." + "\nYou have paid $" + str(paid) + "\nYou currently owe $" + str(unpaid)
+    print(update)
+elif see_details == 'n' or see_details == 'N':
+    print("OK. Have a good day")
+else:
+    print("Hehe. That's a no-no. You must enter Y or N.")
