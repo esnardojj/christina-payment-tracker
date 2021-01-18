8dc506ced4991877d62ee803ef673e41b7ec06b8
julesnardo <esnardojuliet@gmail.com>
2021-01-18 18:26:59 -0500

    Want to display class

--- xtina.py
+++ xtina.py
@@ -1,24 +1,50 @@
+from Airbnb import Airbnb
 import datetime
 
 # Set date
 date = datetime.datetime.now()
 
 # Set grand total cost
-cost = 1146
-paid = float(146)
+cost = 1263
+paid = float(146*2)
 unpaid = float(cost - paid)
 
 # Set payments
-payments = str(1)
+payments = str(2)
+
+# Prompt 1
+prompt_one = ("\nPlease type the number that matches the action you'd like to do today: \n(1) See Payment Details\n(2) See Airbnb Costs")
+
+# Airbnb list
+airbnb = [
+    Airbnb("Pittsburgh", 0),
+    Airbnb("Chicago", 867),
+    Airbnb("Nashville", 50),
+    Airbnb("New Orleans", 120),
+    Airbnb("Panama City", 50),
+    Airbnb("Savannah", 176)
+]
+
+airbnb_info = (airbnb[0].location, airbnb[0].cost)
+
+for item in airbnb:
+    print(airbnb_info, end=" ")
+
+print("\n")
 
 # Greeting everytime
-greeting = "Hi Christina! Would you like to see your payment track? (Y/N) "
-see_details = input(greeting)
+# greeting = "Hi Christina! How are you today?\n"
+# get_response = input(greeting)
 
-if see_details == 'y' or see_details == 'Y':
-    update = "As of " + date.strftime("%x")+":\nYou have made " + payments + " payments." + "\nYou have paid $" + str(paid) + "\nYou currently owe $" + str(unpaid)
-    print(update)
-elif see_details == 'n' or see_details == 'N':
-    print("OK. Have a good day")
-else:
-    print("Hehe. That's a no-no. You must enter Y or N.")
+# Prompts
+# def prompts():
+#     response = input("Awesome!" + prompt_one + "\n")
+#     if response == "1":
+#         update = "As of " + date.strftime("%x")+":\nYou have made " + payments + " payments." + "\nYou have paid $" + str(paid) + "\nYou currently owe $" + str(unpaid)
+#         print(update)
+#     elif response == "2":
+#         print('\n'.join(map(str, airbnb[0:5])))
+#     else:
+#         print("Run command again or have a good day!")
+
+# print(prompts())
