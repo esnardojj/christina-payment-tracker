a91e4682345702820c7efe2059b48713ca30a44a
julesnardo <esnardojuliet@gmail.com>
2021-01-20 18:36:03 -0500

    made list for airbnb

--- xtina.py
+++ xtina.py
@@ -1,50 +1,52 @@
 from Airbnb import Airbnb
 import datetime
+from git_pull import pull
 
 # Set date
 date = datetime.datetime.now()
 
 # Set grand total cost
 cost = 1263
-paid = float(146*2)
+paid = float(146*3)
 unpaid = float(cost - paid)
 
 # Set payments
-payments = str(2)
+payments = str(3)
 
 # Prompt 1
 prompt_one = ("\nPlease type the number that matches the action you'd like to do today: \n(1) See Payment Details\n(2) See Airbnb Costs")
 
 # Airbnb list
-airbnb = [
-    Airbnb("Pittsburgh", 0),
-    Airbnb("Chicago", 867),
-    Airbnb("Nashville", 50),
-    Airbnb("New Orleans", 120),
-    Airbnb("Panama City", 50),
-    Airbnb("Savannah", 176)
-]
+airbnbList = ['Pittsburgh', '$0', 'Chicago', '$867', 'Nashville', '$50', 'New Orleans', '$120', 'Panama City', '$50', 'Savannah', '$176']
+# Airbnb("Pittsburgh", 0),
+#     Airbnb("Chicago", 867),
+#     Airbnb("Nashville", 50),
+#     Airbnb("New Orleans", 120),
+#     Airbnb("Panama City", 50),
+#     Airbnb("Savannah", 176)
 
-airbnb_info = (airbnb[0].location, airbnb[0].cost)
+# airbnb_info = (airbnb[0].location, airbnb[0].cost)
 
-for item in airbnb:
-    print(airbnb_info, end=" ")
+# for item in airbnb:
+#     print(airbnb_info, end=" ")
 
-print("\n")
+# print("\n")
+
+pull()
+print("Everything's up to date!")
 
 # Greeting everytime
-# greeting = "Hi Christina! How are you today?\n"
-# get_response = input(greeting)
+greeting = "Hi Christina! How are you today?\n"
+get_response = input(greeting)
 
 # Prompts
-# def prompts():
-#     response = input("Awesome!" + prompt_one + "\n")
-#     if response == "1":
-#         update = "As of " + date.strftime("%x")+":\nYou have made " + payments + " payments." + "\nYou have paid $" + str(paid) + "\nYou currently owe $" + str(unpaid)
-#         print(update)
-#     elif response == "2":
-#         print('\n'.join(map(str, airbnb[0:5])))
-#     else:
-#         print("Run command again or have a good day!")
+def prompts():
+    response = input("Awesome!" + prompt_one + "\n")
+    if response == "1":
+        print("As of " + date.strftime("%x")+":\nYou have made " + payments + " payments." + "\nYou have paid $" + str(paid) + "\nYou currently owe $" + str(unpaid))
+    elif response == "2":
+        print('[%s]' % '\n'.join(map(str, airbnbList)))
+    else:
+        print("Run command again or have a good day!")
 
-# print(prompts())
+print(prompts())
